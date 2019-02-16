8df6151ba531046d71deb82e62e66aaa2d937cf4
Achraf KHAZRI <A.khazri@openbee.com>
2019-02-16 23:22:10 +0100

    Digits detection

--- analogic_clock_reader/rotate.py
+++ analogic_clock_reader/rotate.py
@@ -11,13 +11,13 @@ opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
 contours, hierarchy = cv2.findContours(opening,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 # computes the bounding box for the contour, and draws it on the frame,
 
-for contour, hier in zip(contours, hierarchy):
+for contour in contours:
     (x,y,w,h) = cv2.boundingRect(contour)
     print(x,y,w,h)
-    if w < 80 and h < 80:
-        cv2.rectangle(opening, (x,y), (x+w,y+h), (255, 0, 0), 2)
+    if 5 < w < 80 and 5 < h < 80:
+        cv2.rectangle(img, (x,y), (x+w,y+h), (255, 0, 0), 2)
         print(x,y,w,h)
 
-cv2.imshow("img",opening)
+cv2.imshow("img",img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
