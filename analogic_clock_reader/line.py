6ed39e3e4b337705654c36cc6c0acc51fd3b5cf5
Achraf KHAZRI <A.khazri@openbee.com>
2019-02-17 16:49:53 +0100

    Line detection modification

--- analogic_clock_reader/line.py
+++ analogic_clock_reader/line.py
@@ -12,11 +12,14 @@ ret,thresh = cv2.threshold(edges,127,255,cv2.THRESH_BINARY)
 
 lines = cv2.HoughLines(thresh,1,np.pi/180,70)
 
+kernel = np.ones((20,20),np.uint8)
+
 xx1 = 0
 xx2 = 0
 yy1 = 0
 yy2 = 0
 
+img2 = np.ones((255,255),dtype=np.uint8) * 255
 
 for i in range(len(lines)):
     for rho,theta in lines[i]:
@@ -30,18 +33,33 @@ for i in range(len(lines)):
         y2 = int(y0 - 1000*(a))
 
 
+        img3 = np.ones(gray.shape,dtype=np.uint8) * 255
         if ((abs(x2) >  abs(xx2) + 10)):
-            cv2.line(img,(x1,y1),(x2,y2),(0,0,255),2)
-            print(x1,y1)
-            """im = cv2.getRectSubPix(thresh, (y1, y2), (x1, x2))
-            cv.imshow('Img', im)
-            cv.waitKey(0)"""
+            cv2.line(img3,(x1,y1),(x2,y2),(0,0,255),2)
+            img3 = cv2.morphologyEx(img3, cv2.MORPH_OPEN, kernel)
+            """contours, hierarchy = cv2.findContours(img3,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+            for contour in contours:
+                (x,y,w,h) = cv2.boundingRect(contour)
+                cv2.drawContours(img, contours, -1, (0,255,0), 3)
+                ret,thresh = cv2.threshold(img3,0,1,cv2.THRESH_BINARY)
+                #cv2.rectangle(img, (x,y), (x+w,y+h), (0, 255, 0), 2)
+                print(thresh)"""
+
+            gray = gray * img3
+            #cv2.imshow('Img', img3)
+            cv2.imshow('Img', gray)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
+            print(x2,x1,y2,y1)
         xx1 = x1
         xx2 = x2
         yy1 = y1
         yy2 = y2
 
 
+# computes the bounding box for the contour, and draws it on the frame,
+
+"""
 cv2.imshow("img",img)
 cv2.waitKey(0)
-cv2.destroyAllWindows()
+cv2.destroyAllWindows()"""
