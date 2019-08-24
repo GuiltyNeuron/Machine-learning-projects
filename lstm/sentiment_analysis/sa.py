dfc7a886499756e32920eac1a6e607b26c013f41
Achraf KHAZRI <A.khazri@openbee.com>
2019-08-24 15:41:47 +0100

    :construction :hummer: add a mthod for text processing

--- lstm/sentiment_analysis/sa.py
+++ lstm/sentiment_analysis/sa.py
@@ -3,6 +3,43 @@ import re
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 
+
+# Method for text processing
+def process_txt(data_list):
+
+    data_processed = []
+    for item in data_list:
+
+        # Remove one char tokens
+        text = re.sub(r'\b\w{1,1}\b', '', item[0])
+
+        # Remove special chars
+        special_chars = [',', '.', '"', ':', ')', '(', '-', '!', '?', '|', ';', "'", '$', '&', '/', '[', ']', '>', '%',
+                         '=',
+                         '#', '*', '+', '\\', '@', '_  ']
+        for sp in special_chars:
+            if sp in text:
+                text = text.replace(sp, ' ')
+
+        # Remove digits
+        text = re.sub("\d", " ", text)
+
+        # To lower case
+        text = text.lower()
+
+        # Tokenize text
+        tokens = word_tokenize(str(text))
+
+        # Remove english stop words
+        filtered = [word for word in tokens if word not in stopwords.words('english')]
+
+        # Rejoin tokens
+        text = " ".join(filtered)
+
+        data_processed.append(text)
+    return data_processed
+
+
 # Load data from csv files
 train = pd.read_csv("train.csv", encoding = "ISO-8859-1")
 test = pd.read_csv("test.csv", encoding = "ISO-8859-1")
@@ -16,34 +53,10 @@ test_data = test.iloc[:, 5:6].values
 test_labels = test.iloc[:, 0:1].values
 
 # Text preprocessing
-test_data_processed = []
-for item in test_data:
+test_data_processed = process_txt(test_data)
+train_data_processed = process_txt(train_data)
 
-    # Remove one char tokens
-    text = re.sub(r'\b\w{1,1}\b', '', item[0])
+print(test_data_processed)
 
-    # Remove special chars
-    special_chars = [',', '.', '"', ':', ')', '(', '-', '!', '?', '|', ';', "'", '$', '&', '/', '[', ']', '>', '%', '=',
-                     '#', '*', '+', '\\', '@', '_  ']
-    for sp in special_chars:
-        if sp in text:
-            text = text.replace(sp, ' ')
-
-    # Remove digits
-    text = re.sub("\d", " ", text)
-
-    # To lower case
-    text = text.lower()
-
-    # Tokenize text
-    tokens = word_tokenize(str(text))
-
-    # Remove english stop words
-    filtered = [word for word in tokens if word not in stopwords.words('english')]
-
-    # Rejoin tokens
-    text = " ".join(filtered)
-
-    test_data_processed.append(text)
 
 
